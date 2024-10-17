6b047fb9e98c0d75b49c5aca0fc9be6ac9003dc2
Tejas Panchal <tejaspanchal127@gmail.com>
2024-10-17 23:41:46 +0530

    initial commit

---
+++ src/main/java/com/bank/atm/simulator/BankAtmSimulatorApplication.java
@@ -0,0 +1,13 @@
+package com.bank.atm.simulator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankAtmSimulatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankAtmSimulatorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bank/atm/simulator/BankAtmSimulatorApplicationTests.java
@@ -0,0 +1,14 @@
+package com.bank.atm.simulator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankAtmSimulatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+
+	}
+
+}
